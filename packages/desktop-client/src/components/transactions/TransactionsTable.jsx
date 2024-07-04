192411c5a378e5603899a07bad5d1878c0eab280
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-07-04 13:34:51 -0700

    More components to use react aria Button

--- packages/desktop-client/src/components/transactions/TransactionsTable.jsx
+++ packages/desktop-client/src/components/transactions/TransactionsTable.jsx
@@ -61,7 +61,7 @@ import { styles, theme } from '../../style';
 import { AccountAutocomplete } from '../autocomplete/AccountAutocomplete';
 import { CategoryAutocomplete } from '../autocomplete/CategoryAutocomplete';
 import { PayeeAutocomplete } from '../autocomplete/PayeeAutocomplete';
-import { Button } from '../common/Button';
+import { Button } from '../common/Button2';
 import { Popover } from '../common/Popover';
 import { Text } from '../common/Text';
 import { View } from '../common/View';
@@ -447,8 +447,9 @@ function HeaderCell({
       }}
       unexposedContent={({ value: cellValue }) => (
         <Button
-          type="bare"
-          onClick={onClick}
+          variant="bare"
+          aria-label={`Sort by ${id}`}
+          onPress={onClick}
           style={{
             whiteSpace: 'nowrap',
             overflow: 'hidden',
@@ -636,13 +637,13 @@ function PayeeIcons({
     return null;
   }
 
-  const buttonStyle = {
+  const buttonStyle = ({ isHovered }) => ({
     marginLeft: -5,
     marginRight: 2,
     width: 23,
     height: 23,
-    color: 'inherit',
-  };
+    color: isHovered ? theme.tableText : theme.tableTextSubdued,
+  });
 
   const scheduleIconStyle = { width: 13, height: 13 };
 
@@ -654,10 +655,10 @@ function PayeeIcons({
     <>
       {schedule && (
         <Button
-          type="bare"
+          variant="bare"
+          aria-label="See schedule details"
           style={buttonStyle}
-          onClick={e => {
-            e.stopPropagation();
+          onPress={() => {
             onNavigateToSchedule(scheduleId);
           }}
         >
@@ -670,11 +671,10 @@ function PayeeIcons({
       )}
       {transferAccount && (
         <Button
-          type="bare"
-          aria-label="Transfer"
+          variant="bare"
+          aria-label="See transfer account"
           style={buttonStyle}
-          onClick={e => {
-            e.stopPropagation();
+          onPress={() => {
             if (!isTemporaryId(transaction.id)) {
               onNavigateToTransferAccount(transferAccount.id);
             }
@@ -1463,18 +1463,18 @@ function TransactionError({
             </Text>
             <View style={{ flex: 1 }} />
             <Button
-              type="normal"
+              variant="normal"
               style={{ marginLeft: 15 }}
-              onClick={onDistributeRemainder}
+              onPress={onDistributeRemainder}
               data-testid="distribute-split-button"
-              disabled={!canDistributeRemainder}
+              isDisabled={!canDistributeRemainder}
             >
               Distribute
             </Button>
             <Button
-              type="primary"
+              variant="primary"
               style={{ marginLeft: 10, padding: '4px 10px' }}
-              onClick={onAddSplit}
+              onPress={onAddSplit}
               data-testid="add-split-button"
             >
               Add Split
@@ -1599,7 +1599,7 @@ function NewTransaction({
       >
         <Button
           style={{ marginRight: 10, padding: '4px 10px' }}
-          onClick={() => onClose()}
+          onPress={() => onClose()}
           data-testid="cancel-button"
         >
           Cancel
@@ -1616,9 +1616,9 @@ function NewTransaction({
           />
         ) : (
           <Button
-            type="primary"
+            variant="primary"
             style={{ padding: '4px 10px' }}
-            onClick={onAdd}
+            onPress={onAdd}
             data-testid="add-button"
           >
             Add
@@ -2343,9 +2343,9 @@ function notesTagFormatter(notes, onNotesTagClick) {
             return (
               <span key={`${validTag}${ti}`}>
                 <Button
-                  type="bare"
+                  variant="bare"
                   key={i}
-                  style={{
+                  style={({ isHovered }) => ({
                     display: 'inline-flex',
                     padding: '3px 7px',
                     borderRadius: 16,
@@ -2353,13 +2353,11 @@ function notesTagFormatter(notes, onNotesTagClick) {
                     backgroundColor: theme.noteTagBackground,
                     color: theme.noteTagText,
                     cursor: 'pointer',
-                  }}
-                  hoveredStyle={{
-                    backgroundColor: theme.noteTagBackgroundHover,
-                    color: theme.noteTagText,
-                  }}
-                  onClick={e => {
-                    e.stopPropagation();
+                    ...(isHovered
+                      ? { backgroundColor: theme.noteTagBackgroundHover }
+                      : {}),
+                  })}
+                  onPress={() => {
                     onNotesTagClick?.(validTag);
                   }}
                 >
